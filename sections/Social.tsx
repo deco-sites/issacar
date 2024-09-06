b8b3a925f0003a3b293f94c58949810d5f65007b
Paulo Vieira <paulogarcia-vieira@outlook.com>
2024-09-06 15:26:16 -0300

    Publish 4 from localhost into origin/main

--- sections/Social.tsx
+++ sections/Social.tsx
@@ -18,37 +18,39 @@ export default function Social(
     },
 ) {
     return (
-        <>
-            {content && content.items && content.items.length > 0 && (
-                <div class="flex flex-col gap-4">
-                    {content.title && <h3 class="text-lg">{content.title}</h3>}
-                    <ul
-                        class={`flex gap-4 ${vertical ? "lg:flex-col lg:items-start" : "flex-wrap items-center"
-                            }`}
-                    >
-                        {content.items.map((item) => {
-                            return (
-                                <li>
-                                    <a
-                                        href={item.link}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        aria-label={`${item.label} Logo`}
-                                        class="flex gap-2 items-center"
-                                    >
-                                        <span class="block p-1 border rounded-full">
-                                            <Icon size={24} id={item.label} />
-                                        </span>
-                                        {vertical && (
-                                            <div class="text-sm hidden lg:block">{item.label}</div>
-                                        )}
-                                    </a>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-            )}
-        </>
+        <nav class="lg:container lg:mx-auto mx-4">
+            <div class="flex flex-col items-center gap-8">
+                {content && content.items && content.items.length > 0 && (
+                    <div class="flex flex-col gap-4">
+                        {content.title && <h3 class="text-4xl leading-snug">{content.title}</h3>}
+                        <ul
+                            class={`flex gap-4 ${vertical ? "lg:flex-col lg:items-start" : "flex-wrap items-center"
+                                }`}
+                        >
+                            {content.items.map((item) => {
+                                return (
+                                    <li>
+                                        <a
+                                            href={item.link}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            aria-label={`${item.label} Logo`}
+                                            class="flex gap-2 items-center"
+                                        >
+                                            <span class="block p-1 border rounded-full">
+                                                <Icon size={40} id={item.label} />
+                                            </span>
+                                            {vertical && (
+                                                <div class="text-sm hidden lg:block">{item.label}</div>
+                                            )}
+                                        </a>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                )}
+            </div>
+        </nav>
     );
 }
