dd4067392041de463c3af7272aaa472770a6d971
PauloIletski <paulogarcia-vieira@outlook.com>
2024-09-17 19:04:46 -0300

    feat: add map feature at card

--- sections/Maps.tsx
+++ sections/Maps.tsx
@@ -1,16 +1,109 @@
-export interface Props {
-    api?: string;
+export interface CTA {
+    id?: string;
+    href: string;
+    text: string;
+    style?: "Outline" | "Ghost";
 }
 
-export default function Maps({ api = "" }: Props) {
+export interface Props {
+    title?: string;
+    /** @format textarea */
+    description?: string;
+    tagline?: string;
+    showMap?: boolean;
+    api?: string;
+    placement?: "left" | "right" | "top" | "bottom";
+    cta?: CTA[];
+    disableSpacing?: {
+        top?: boolean;
+        bottom?: boolean;
+    };
+
+}
+
+const PLACEMENT = {
+    left: "flex-col md:flex-row-reverse",
+    right: "flex-col md:flex-row",
+    top: "flex-col md:flex-col sm:flex-col",
+    bottom: "flex-col-reverse md:flex-col-reverse sm:flex-col-reverse"
+};
+
+export default function Maps({
+    title = "Here's an intermediate size heading you can edit",
+    description =
+    "This text is fully editable and ready for your personal touch. Just click here, head over to the section window, or dive straight into the code to make changes as you see fit. Whether it's about the content, formatting, font, or anything in between, editing is just a click away.",
+    tagline = "Tagline",
+    api = '',
+    showMap = true,
+    placement = "left",
+    disableSpacing,
+    cta = [
+        { id: "change-me-1", href: "/", text: "Change me", style: "Outline" },
+        { id: "change-me-2", href: "/", text: "Change me", style: "Ghost" },
+    ],
+}: Props) {
     return (
-        <iframe
+        <div class="lg:container md:max-w-6xl lg:mx-auto mx-4 text-sm">
+            <div
+                class={`flex ${PLACEMENT[placement]
+                    } gap-12 md:gap-20 text-left items-center z-10 ${disableSpacing?.top ? "" : "pt-12 lg:pt-28"
+                    } ${disableSpacing?.bottom ? "" : "pb-12 lg:pb-28"}`}
+            >
+                <div class="w-full md:w-1/2 border border-secondary rounded-lg overflow-hidden">
+                    {showMap && <iframe
             width="600"
             height="450"
             style={{ border: 0 }}
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
-            src={`https://www.google.com/maps/embed/v1/place?key=${api}&q=Space+Needle,Seattle+WA`}
-        ></iframe>
+                        src={`https://www.google.com/maps/embed/v1/place?key=${api}&q=Issacar+Church+Lugar+de+Adoração`}
+                        class='w-100 flex justify-center'
+                    />
+                    }
+                </div>
+                <div class="w-full md:w-1/2 space-y-2 md:space-y-4 md:max-w-xl gap-4 z-10">
+                    <p class="text-sm font-semibold">
+                        {tagline}
+                    </p>
+                    <p class="text-4xl leading-snug">
+                        {title}
+                    </p>
+                    <p class="leading-normal">
+                        {description}
+                    </p>
+                    <div class="flex gap-3 pt-4">
+                        {cta.length > 0 && cta?.map((item) => (
+                            <a
+                                key={item?.id}
+                                id={item?.id}
+                                href={item?.href}
+                                target={item?.href.includes("http") ? "_blank" : "_self"}
+                                class={`font-normal btn btn-primary
+                    ${!item.style || item.style == "Outline" && "btn-outline"}
+                    ${item.style == "Ghost" && "btn-ghost"}
+                  `}
+                            >
+                                {item?.text}
+                                {item.style == "Ghost" && (
+                                    <svg
+                                        width="24"
+                                        height="25"
+                                        viewBox="0 0 24 25"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M9.70697 16.9767L15.414 11.2697L9.70697 5.56274L8.29297 6.97674L12.586 11.2697L8.29297 15.5627L9.70697 16.9767Z"
+                                            fill="#18181B"
+                                        />
+                                    </svg>
+                                )}
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+
     );
 }
