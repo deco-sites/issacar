0187f4d93a1d761a30551f7897737390dd16721f
Paulo Vieira <paulogarcia-vieira@outlook.com>
2024-09-27 18:57:42 +0000

    Publish 4 from Homologation into origin/main

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,10 +2,11 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$$$$$$0 from "./apps/deco/analytics.ts";
-import * as $$$$$$$$$$$1 from "./apps/deco/records.ts";
-import * as $$$$$$$$$$$2 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$3 from "./apps/site.ts";
+import * as $$$$$$$$$$$0 from "./apps/deco/ai-assistants.ts";
+import * as $$$$$$$$$$$1 from "./apps/deco/analytics.ts";
+import * as $$$$$$$$$$$2 from "./apps/deco/records.ts";
+import * as $$$$$$$$$$$3 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$4 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/BlogPosts.tsx";
 import * as $$$$$$1 from "./sections/CopyContentSession.tsx";
 import * as $$$$$$2 from "./sections/Faq.tsx";
@@ -41,10 +42,11 @@ const manifest = {
     "site/sections/Theme/Theme.tsx": $$$$$$11,
   },
   "apps": {
-    "site/apps/deco/analytics.ts": $$$$$$$$$$$0,
-    "site/apps/deco/records.ts": $$$$$$$$$$$1,
-    "site/apps/decohub.ts": $$$$$$$$$$$2,
-    "site/apps/site.ts": $$$$$$$$$$$3,
+    "site/apps/deco/ai-assistants.ts": $$$$$$$$$$$0,
+    "site/apps/deco/analytics.ts": $$$$$$$$$$$1,
+    "site/apps/deco/records.ts": $$$$$$$$$$$2,
+    "site/apps/decohub.ts": $$$$$$$$$$$3,
+    "site/apps/site.ts": $$$$$$$$$$$4,
   },
   "name": "site",
   "baseUrl": import.meta.url,
