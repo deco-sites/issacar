81851d3b5402df850b786a1ba9a3e55efb5dc598
PauloIletski <paulogarcia-vieira@outlook.com>
2024-09-27 17:52:46 -0300

    feat: commit last changes

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -13,18 +13,19 @@ import * as $$$$$$1 from "./sections/CopyContentSession.tsx";
 import * as $$$$$$2 from "./sections/Faq.tsx";
 import * as $$$$$$3 from "./sections/Footer.tsx";
 import * as $$$$$$4 from "./sections/Header.tsx";
-import * as $$$$$$14 from "./sections/hero-1.tsx";
-import * as $$$$$$15 from "./sections/hero-2.tsx";
+import * as $$$$$$15 from "./sections/hero-1.tsx";
+import * as $$$$$$16 from "./sections/hero-2.tsx";
 import * as $$$$$$5 from "./sections/Hero.tsx";
-import * as $$$$$$16 from "./sections/hero2.tsx";
-import * as $$$$$$6 from "./sections/IASection.tsx";
-import * as $$$$$$7 from "./sections/ImageWithParagraph.tsx";
-import * as $$$$$$8 from "./sections/Logos.tsx";
-import * as $$$$$$9 from "./sections/Maps.tsx";
-import * as $$$$$$10 from "./sections/NewSection.tsx";
-import * as $$$$$$11 from "./sections/Social.tsx";
-import * as $$$$$$12 from "./sections/Testimonials.tsx";
-import * as $$$$$$13 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$17 from "./sections/hero2.tsx";
+import * as $$$$$$6 from "./sections/HeroNormal.tsx";
+import * as $$$$$$7 from "./sections/IASection.tsx";
+import * as $$$$$$8 from "./sections/ImageWithParagraph.tsx";
+import * as $$$$$$9 from "./sections/Logos.tsx";
+import * as $$$$$$10 from "./sections/Maps.tsx";
+import * as $$$$$$11 from "./sections/NewSection.tsx";
+import * as $$$$$$12 from "./sections/Social.tsx";
+import * as $$$$$$13 from "./sections/Testimonials.tsx";
+import * as $$$$$$14 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
@@ -33,18 +34,19 @@ const manifest = {
     "site/sections/Faq.tsx": $$$$$$2,
     "site/sections/Footer.tsx": $$$$$$3,
     "site/sections/Header.tsx": $$$$$$4,
-    "site/sections/hero-1.tsx": $$$$$$14,
-    "site/sections/hero-2.tsx": $$$$$$15,
+    "site/sections/hero-1.tsx": $$$$$$15,
+    "site/sections/hero-2.tsx": $$$$$$16,
     "site/sections/Hero.tsx": $$$$$$5,
-    "site/sections/hero2.tsx": $$$$$$16,
-    "site/sections/IASection.tsx": $$$$$$6,
-    "site/sections/ImageWithParagraph.tsx": $$$$$$7,
-    "site/sections/Logos.tsx": $$$$$$8,
-    "site/sections/Maps.tsx": $$$$$$9,
-    "site/sections/NewSection.tsx": $$$$$$10,
-    "site/sections/Social.tsx": $$$$$$11,
-    "site/sections/Testimonials.tsx": $$$$$$12,
-    "site/sections/Theme/Theme.tsx": $$$$$$13,
+    "site/sections/hero2.tsx": $$$$$$17,
+    "site/sections/HeroNormal.tsx": $$$$$$6,
+    "site/sections/IASection.tsx": $$$$$$7,
+    "site/sections/ImageWithParagraph.tsx": $$$$$$8,
+    "site/sections/Logos.tsx": $$$$$$9,
+    "site/sections/Maps.tsx": $$$$$$10,
+    "site/sections/NewSection.tsx": $$$$$$11,
+    "site/sections/Social.tsx": $$$$$$12,
+    "site/sections/Testimonials.tsx": $$$$$$13,
+    "site/sections/Theme/Theme.tsx": $$$$$$14,
   },
   "apps": {
     "site/apps/deco/ai-assistants.ts": $$$$$$$$$$$0,
