fad74e58c1b013b68354420e6315e32688c99a82
Paulo Vieira <paulogarcia-vieira@outlook.com>
2024-11-26 21:12:47 +0000

    Publish 5 from Homologation into origin/main

--- islands/CopyContent.tsx
+++ islands/CopyContent.tsx
@@ -1,31 +1,38 @@
-import { useState } from "preact/hooks";
-
 export interface Props {
+
+  /**
+   * @description The text content to be copied
+   * @format textarea
+   */
   contentCopy: string;
+
+  /**
+   * @description The text for the copy button
+   */
+  buttonText ?: string;
+
 }
 
 export default function Copy({
   contentCopy = "",
+  buttonText = "Copiar",
 }: Props) {
-  const [copySuccess, setCopySuccess] = useState(""); // Estado para mensagem de sucesso
+ 
 
   // Função para copiar texto
   const copyToClipboard = async () => {
     try {
       await navigator.clipboard.writeText(contentCopy);
-      setCopySuccess("Texto copiado com sucesso!"); // Exibe a mensagem de sucesso
+      alert("Texto copiado com sucesso!"); // Exibe a mensagem de sucesso
     } catch (err) {
       console.info("ERRO", err);
-      setCopySuccess("Falha ao copiar o texto."); // Exibe a mensagem de erro
     }
   };
 
   return (
-    <div>
-      <p>{contentCopy}</p>
-      <button onClick={copyToClipboard}>Copiar</button>
-      {/* Mensagem de sucesso ou erro */}
-      {copySuccess && <p>{copySuccess}</p>}
+    <div class="bg-base-200 p-4 rounded-lg shadow-md max-w-md mx-auto justify-center">
+      <p class="text-4xl font-black mb-8">{contentCopy}</p>
+      <button class="btn btn-primary" onClick={copyToClipboard}>{buttonText}</button>
     </div>
-  );
+  )
 }
--- sections/CopyContentSession.tsx
+++ sections/CopyContentSession.tsx
@@ -1,12 +1,38 @@
 import CopyContent from "site/islands/CopyContent.tsx";
 
 export interface Props {
-  
+  /**  
+   * @description The content to be copied
+   * @format string
+  */
   contentCopy: string;
+  /**
+   * @description The background color of the copy section
+   * @format color-input
+   */
+  backgroundColor?: string;
+/**  
+   * @description The content to be copied
+   * @format string
+  */
+
+  buttonTextValue?:string 
+
+
 }
 
 export default function Copy({
   contentCopy = "",
+  backgroundColor="#f3f4f6",
+  buttonTextValue="Copiar"
 }: Props) {
-  return <CopyContent contentCopy={contentCopy} />;
+
+  return (
+    <div class="p-2 rounded-lg" style={{ backgroundColor }}>
+      <CopyContent contentCopy={contentCopy} buttonText={buttonTextValue} />
+    </div>
+   
+  )
+  
+  
 }
