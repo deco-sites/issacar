d23027b7d669001409408df202ce4eb691cedcdd
decobot <capy@deco.cx>
2025-02-07 21:44:41 -0300

    feaure: implement ministeries

--- sections/Logos.tsx
+++ sections/Logos.tsx
@@ -10,6 +10,7 @@ export interface Logo {
 export interface Props {
   title?: string;
   logos?: Logo[];
+  animate:boolean;
 }
 
 const IMG_PLACEHODLER = Array(30).fill(0).map(() => ({
@@ -29,6 +30,7 @@ export const LoadingFallback = () => {
 export default function Logos({
   title = "Edit this heading however you want",
   logos = IMG_PLACEHODLER,
+  animate=true
 }: Props) {
   const slideContent = (
     <div class="flex items-center gap-20">
@@ -38,6 +40,7 @@ export default function Logos({
             src={logo.src || ""}
             alt={logo.altText || ""}
             width={110}
+            height={60}
           />
         );
       })}
@@ -46,9 +49,9 @@ export default function Logos({
   return (
     <div class="lg:container md:max-w-6xl lg:mx-auto mx-4 py-6 lg:py-14">
       <div class="flex flex-col gap-12">
-        <p class="text-center text-lg">{title}</p>
-        <div class="relative w-full overflow-hidden h-11">
-          <div class="animate-sliding absolute top-0 left-0 flex flex-nowrap h-11">
+        <p class="text-center text-4xl">{title}</p>
+        <div class="relative w-full overflow-hidden h-[4rem]">
+          <div class={`${animate?"animate-sliding":"justify-center"} absolute top-0 ${animate?"":"left-0"} flex flex-nowrap h-full`}>
             {slideContent}
           </div>
         </div>
